825540d97affc5ffa56cdc2968c770f6d86eb6ba
Flyingdotschman <dominik.poeschel@hotmail.com>
2021-02-03 12:02:03 +0100

    Logo hinter Video eingefügt

--- main.py
+++ main.py
@@ -66,9 +66,9 @@ if platform.system() != "Windows":
     background_stop = PhotoImage(file="/home/pi/PeopleCounter_V2/Stop.png")
 
     width = (1920-1312)
-    heiht = width
+    height = width
     img = Image.open("/home/pi/PeopleCounter_V2/Logo.png")
-    img = img.resize((width,height), Image.ANTIALIAS)
+    img = img.resize((width, height), Image.ANTIALIAS)
     logo = PhotoImage(img)
 else:
     background_go = PhotoImage(file="Go.png")
