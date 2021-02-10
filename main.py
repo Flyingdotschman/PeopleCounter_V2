b6a26e91f3d9c0c95b5b61c3d3479d6e89599da8
Flyingdotschman <dominik.poeschel@hotmail.com>
2021-02-10 14:01:45 +0100

    video player wait for event t.event. wait line 408

--- main.py
+++ main.py
@@ -405,7 +405,7 @@ def start_video_player():
                 if max_people_reached():
                     video_player.hide_video()
                 t = threading.Event()
-                t.event.wait(video_player.is_playing())
+                t.wait(video_player.is_playing() is False)
                 sleep(3)
                 #sleep(duration_of_video)
         else:
